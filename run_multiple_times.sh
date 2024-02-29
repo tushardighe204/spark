07db0853b23b95bd2a416864e40902b840da1979
Tushar Dighe <64326412+dighetushar654@users.noreply.github.com>
2024-02-29 13:07:54 +0530

    Update run_multiple_times.sh

--- run_multiple_times.sh
+++ run_multiple_times.sh
@@ -3,8 +3,12 @@
 # Number of times to run the Scala application
 num_runs=5
 
-# Loop to run the Scala application multiple times
-for ((i=1; i<=$num_runs; i++)); do
-    echo "Running HelloWorld for the $i time"
-    scala HelloWorld
+# Continuous loop to run the Scala application
+while true; do
+    for ((i=1; i<=$num_runs; i++)); do
+        echo "Running HelloWorld for the $i time"
+        scala HelloWorld
+    done
+    sleep 4000  # Adjust sleep duration as needed
 done
+
